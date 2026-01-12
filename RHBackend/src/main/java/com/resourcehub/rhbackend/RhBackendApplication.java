a5b343fe1e485868378f5a914993ad8a602627ef
Geetha <geethasrikota169@gmail.com>
2026-01-12 16:33:09 +0530

    chore: initial setup of backend and frontend

---
+++ RHBackend/src/main/java/com/resourcehub/rhbackend/RhBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resourcehub.rhbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RhBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RhBackendApplication.class, args);
+	}
+
+}
---
+++ RHBackend/src/test/java/com/resourcehub/rhbackend/RhBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resourcehub.rhbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
